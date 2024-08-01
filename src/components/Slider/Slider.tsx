1b5264fd04892d4b9d280e9d1c4e2a56b1c65fb4
Asker <kotsevasker0@gmail.com>
2024-08-01 21:15:20 +0300

    add func next and prev button

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -18,17 +18,32 @@ function Slider() {
       setCount(sliderProp.length-1)
     }
   }
+  
   return (
     <div className="flex wrap z-[5] items-start">
-    <img src={sliderProp[count].photo} className="w-[608px] z-[10] h-[740px]" alt="" />
+    <img src={sliderProp[count].photo} className="w-[608px]  transition duration-300 z-[10] h-[740px]" alt="" />
     <div className="flex w-[672px] z-[10] h-[740px] justify-end flex-col items-center gap-48">
       <div className="flex z-[10] flex-col max-w-[519px] items-center  px-11 gap-4  justify-center"> 
           <span className="max-w-[523px] transition-all  z-[10] text-[48px] text-primary ">{sliderProp[count].title}</span>
           <p className="text-[18px] z-[10] montserrat font-normal max-w-[493px] text-[#2C1500]">{sliderProp[count].text} </p>
       </div>
       <div className="flex z-[10] items-center justify-center">
-          <button onClick={()=>prevSlide()} className="flex z-[10] hover:bg-primary hover:text-white w-[320px] h-[160px] text-[#999999]  items-center justify-center  transition ease-in-out duration-500"><img src="/public/arrow-left.svg" alt="" /> <span className="text-[24px]">Previous</span> </button>
-          <button onClick={()=>nextSlide()} className=" w-[320px] hover:bg-primary  hover:text-white  z-[10] h-[160px] flex items-center justify-center text-[#999999] transition ease-in-out duration-500"> <span className="text-[24px]">Next</span> <img src="/public/arrow-back-outline.svg" alt="" /></button>
+          <button disabled = { count < 1 ? true : false} onClick={()=>prevSlide()} 
+          className="flex hovbtn z-[10] hover:bg-primary flex-col hover:text-white w-[320px] h-[160px] text-[#999999] px-6  items-start justify-center  transition ease-in-out duration-500">
+          <div className="hstack gap-2">
+          <img src="/public/arrow-left.svg" alt="" /> 
+          <span className="text-[24px]">Previous</span> 
+          </div>
+          <span className="btn transition duration-300">{ count == 0 ? sliderProp[count].title : sliderProp[count-1].title}</span>
+          </button>
+          <button  onClick={()=>nextSlide()} 
+          className=" hovbtn w-[320px] hover:bg-primary px-6  hover:text-white  z-[10] h-[160px] flex flex-col items-start justify-center text-[#999999] transition ease-in-out duration-500">
+            <div className="hstack gap-2">
+            <span className="text-[24px]">Next</span> 
+            <img src="/public/arrow-back-outline.svg" alt="" />
+            </div>
+            <span className="btn  transition duration-300">{ count == sliderProp.length-1  ?  sliderProp[count].title : sliderProp[count+1].title}</span>
+            </button>
       </div>
     </div>
     </div>  
